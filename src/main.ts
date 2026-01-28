49c9b37c32ff92d60285973651f543dbabf31c51
vinybergamo <vinybergamo@gmail.com>
2026-01-28 10:53:09 -0300

    chore: Update initial credit display to a loading state and log server port on startup.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000).then(() => {
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  });
 }
 bootstrap();
